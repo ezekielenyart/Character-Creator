ff9ee48f41e3d9aee9a742fff166755412f9dc89
Nathan Szurek <nathan.szurek@yahoo.com>
2020-08-08 23:53:11 -0500

    Routing files created

---
+++ Routes/externalApiRoutes.js
@@ -0,0 +1,1 @@
+// Routes for getting autofill data used to create and update character sheets from D&D 5e API. For more info check out https://www.dnd5eapi.co/
---
+++ Routes/internalApiRoutes.js
@@ -0,0 +1,1 @@
+// Routes for getting autofill data used to create and update character sheets from our own api built from user created content (this is planned for future implementation).
