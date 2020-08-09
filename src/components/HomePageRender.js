34538af76a27610dbf43a8f28ab58dfa5bab9a9b
Ezekiel <zekejenyart@gmail.com>
2020-08-08 19:08:12 -0500

    React server connected

--- src/components/HomePageRender.js
+++ src/components/HomePageRender.js
@@ -1,13 +1,13 @@
 import React from "react";
 import LogRegBar from "./LogRegBar"
-
+import CharacterCreateBtn from "./CreateCharacterBtn"
 
 
 function HomePageRender() {
   return (
       <div>
     <LogRegBar />
-    <CreateCharacterBtn />
+    <CharacterCreateBtn />
     </div>
   );
 }
