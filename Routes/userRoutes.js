f2522b15fcbcd778b346964e02a83b83c87224c2
Nathan Szurek <nathan.szurek@yahoo.com>
2020-08-09 21:28:42 -0500

    Created user api routes

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,2 +1,85 @@
 // Routes for creating, updating, and deleting users.
-const db = require('../models')
+const db = require('../models');
+const passport = require('../config/passport')
+
+module.exports = function(app) {
+    app.post("/api/login", passport.authenticate("local"), function(req, res) {
+      res.json(req.user);
+    });
+  
+    app.get("/logout", (req, res) => {
+      req.logout();
+      console.log("You've been logged out")
+      res.redirect("/");
+    });
+  
+    // Route for getting some data about our user to be used client side
+    app.get("/api/user_data", function(req, res) {
+      if (!req.user) {
+        // The user is not logged in, send back an empty object
+        res.json({});
+      } else {
+        // Otherwise send back the user's username and id
+        // Sending back a password, even a hashed password, isn't a good idea
+        res.json({
+          username: req.user.username,
+          id: req.user.id
+        });
+      }
+    });
+  
+    app.get("/api/user", function(req, res) {
+      // Here we add an "include" property to our options in our findAll query
+      // We set the value to an array of the models we want to include in a left outer join
+      // In this case, just db.Character
+      db.User.findAll({
+        include: [db.Character]
+      }).then(function(dbUser) {
+        res.json(dbUser);
+      }).catch(err => console.log(err))
+    });
+  
+    app.get("/api/user/:id", function(req, res) {
+      // Here we add an "include" property to our options in our findOne query
+      // We set the value to an array of the models we want to include in a left outer join
+      // In this case, just db.Character
+      db.User.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [db.Character]
+      }).then(function(dbUser) {
+        res.json(dbUser);
+      }).catch(err => console.log(err))
+    });
+  
+    app.post("/api/user", function(req, res) {
+      db.User.create(req.body).then(function(dbUser) {
+        res.json(dbUser);
+      }).catch(err => console.log(err))
+    });
+  
+    app.delete("/api/user/:id", function(req, res) {
+      db.User.destroy({
+        where: {
+          id: req.params.id
+        }
+      }).then(function(dbUser) {
+        res.json(dbUser);
+      }).catch(err => console.log(err))
+    });
+  
+    // PUT route for updating users
+    app.put("/api/user", function(req, res) {
+      db.User.update(req.body,
+        {
+          where: {
+            id: req.body.userId
+          }
+        })
+        .then(function(dbUser) {
+          res.json(dbUser);
+        })
+        .catch(err => console.log(err))
+    });
+  };
