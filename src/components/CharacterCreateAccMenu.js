8f839cb73beb0c556240168595891017ab8b4dab
Ezekiel <zekejenyart@gmail.com>
2020-08-10 21:30:16 -0500

    Added basic Accordion menu

--- src/components/CharacterCreateAccMenu.js
+++ src/components/CharacterCreateAccMenu.js
@@ -2,17 +2,233 @@ import React from "react";
 
 function CharacterCreateAccMenu() {
     return (
-        <div>
-            <ul class="vertical menu accordion-menu" data-accordion-menu>
-                <li>
-                    <a href="#">Item 1</a>
-                    <ul class="menu vertical nested">
-                        <li><a href="#">Item 1A</a></li>
-                        <li><a href="#">Item 1B</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Item 2</a></li>
-            </ul>
+<div className="container mt-5">
+        <div className="panel-group" id="accordion">
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+                            Race</a>
+                    </h4>
+                </div>
+                <div id="collapse1" className="panel-collapse collapse ">
+                    <div className="panel-body">
+                        Human
+                    </div>
+                    <div className="panel-body">
+                        Elf
+                    </div>
+                    <div className="panel-body">
+                        Dwarves
+                    </div>
+                    <div className="panel-body">
+                        Halfling
+                    </div>
+                    <div className="panel-body">
+                        Half-Elf
+                    </div>
+                    <div className="panel-body">
+                        Half-Orc
+                    </div>
+                    <div className="panel-body">
+                        Tiefling
+                    </div>
+                    <div className="panel-body">
+                        Gnome
+                    </div>
+                    <div className="panel-body">
+                        Dragonborn
+                    </div>
+                </div>
+            </div>
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+                            Class
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse2" className="panel-collapse collapse">
+                    <div className="panel-body">
+                        Barbarian
+                    </div>
+                    <div className="panel-body">
+                        Bard
+                    </div>
+                    <div className="panel-body">
+                        Cleric
+                    </div>
+                    <div className="panel-body">
+                        Druid
+                    </div>
+                    <div className="panel-body">
+                        Fighter
+                    </div>
+                    <div className="panel-body">
+                        Monk
+                    </div>
+                    <div className="panel-body">
+                        Paladin
+                    </div>
+                    <div className="panel-body">
+                        Ranger
+                    </div>
+                    <div className="panel-body">
+                        Rogue
+                    </div>
+                    <div className="panel-body">
+                        Sorcerer
+                    </div>
+                    <div className="panel-body">
+                        Warlock
+                    </div>
+                    <div className="panel-body">
+                        Wizard
+                    </div>
+                </div>
+            </div>
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+                            Sex</a>
+                    </h4>
+                </div>
+                <div id="collapse3" className="panel-collapse collapse">
+                    <div className="panel-body">
+                        Male
+                    </div>                    
+                    <div className="panel-body">
+                        Female
+                    </div>
+                </div>
+            </div>
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+                            Education</a>
+                    </h4>
+                </div>
+                <div id="collapse4" className="panel-collapse collapse">
+                    <div className="panel-body">
+                        Classical
+                    </div>
+                    <div className="panel-body">
+                        Spiritual
+                    </div>
+                    <div className="panel-body">
+                        Survival
+                    </div>
+                    <div className="panel-body">
+                        Subsistence
+                    </div>
+                    <div className="panel-body">
+                        Militant
+                    </div>
+                    <div className="panel-body">
+                        Apprenticeship
+                    </div>
+                </div>
+            </div>
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
+                            Background</a>
+                    </h4>
+                </div>
+                <div id="collapse5" className="panel-collapse collapse">
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+                    <div className="panel-body">
+                        Acolyte
+                    </div>
+
+
+                </div>
+            </div>
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse6">
+                            Alignment</a>
+                    </h4>
+                </div>
+                <div id="collapse6" className="panel-collapse collapse">
+                    <div className="panel-body">
+                        Lawful Good
+                    </div>
+                    <div className="panel-body">
+                        Neutral Good
+                    </div>
+                    <div className="panel-body">
+                        Chaotic Good
+                    </div>
+                    <div className="panel-body">
+                        Lawful Neutral
+                    </div>
+                    <div className="panel-body">
+                        True Neutral
+                    </div>
+                    <div className="panel-body">
+                        Chaotic Neutral
+                    </div>
+                    <div className="panel-body">
+                        Lawful Evil
+                    </div>
+                    <div className="panel-body">
+                        Neutral Evil
+                    </div>
+                    <div className="panel-body">
+                        Chaotic Evil
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
     )
-}
+};
+
+export default CharacterCreateAccMenu
