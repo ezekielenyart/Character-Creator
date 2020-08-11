f2eec4243584159fb6f771846722200d843ea78e
Ezekiel <zekejenyart@gmail.com>
2020-08-11 12:43:12 -0500

    Started page routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import HomePageRender from "./components/HomePageRender";
 import CharacterCreateAccMenu from "./components/CharacterCreateAccMenu";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Roster from './components/Roster'
 function App() {
-  return <CharacterCreateAccMenu />;
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={HomePageRender} />
+        <Route exact path="/create" component={CharacterCreateAccMenu} />
+        <Route exact path="/roster" component={Roster} />
+      </div>
+    </Router>
+  
 }
 
 export default App;
