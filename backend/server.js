90262cca065bae006ca8932087b57022fcd9bbc3
Nathan Szurek <nathan.szurek@yahoo.com>
2020-08-13 22:09:43 -0500

    File structure complete

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
-require("./Routes/htmlRoutes.js")
+app.get("/", (req, res) => res.send("hello world"))
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`)
