bee167fabf83fe4592b219bb22b412a642b19e8b
Nathan Szurek <nathan.szurek@yahoo.com>
2020-08-13 21:42:45 -0500

    Minor structure update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React from "react";
 import HomePageRender from "./components/HomePageRender";
 import CharacterCreateAccMenu from "./components/CharacterCreateAccMenu";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-<<<<<<< HEAD
 import Roster from './components/Roster'
-=======
-import Roster from './components/Roster';
 import Wrapper from './components/Wrapper'
->>>>>>> cf2ad471122c7bd812b9c7bad745a16087b6e7b3
 
 function App() {
    return (
