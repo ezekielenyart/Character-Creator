0fb4d19fec7c0c54ca4519ab4ef5987a79dccdc2
Ezekiel <zekejenyart@gmail.com>
2020-08-17 16:21:50 -0500

    New Branch New Me

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Roster from './pages/Roster';
 import Wrapper from './components/Wrapper'
 
-// import CharacterSheet from './pages/CharacterSheet'
+import CharacterSheet from './pages/CharacterSheet'
 // import DeveloperContext from "./components/utils/DeveloperContext";
 import TestAPI from "./components/APItestcomponent.js"
 import DeveloperContext from "./components/utils/DeveloperContext.js";
@@ -43,7 +43,7 @@ function App() {
           <Route exact path="/create" component={CharacterCreateAccMenu} />
           <Route exact path="/roster" component={Roster} />
 
-          {/* <Route exact path="/charactersheet" component={CharacterSheet} /> */}
+          <Route exact path="/charactersheet" component={CharacterSheet} />
           <Route exact path="/api/test" component={TestAPI} />
           </DeveloperContext.Provider>
         </Wrapper>
