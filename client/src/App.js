396cf1184bccb0bf93537fbf0ed737c20301ae40
Ezekiel <zekejenyart@gmail.com>
2020-08-19 11:42:24 -0500

    UserContext WORKSgit add .!

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import HomePageRender from "./pages/HomePageRender";
 import CharacterCreateAccMenu from "./pages/CharacterCreateAccMenu";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Roster from './pages/Roster';
 import Wrapper from './components/Wrapper'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-
+import UserContext from './utils/UserContext'
 import CharacterSheet from './pages/CharacterSheet'
 // import DeveloperContext from "./components/utils/DeveloperContext";
 import TestAPI from "./components/APItestcomponent.js"
@@ -20,13 +20,20 @@ function App() {
       class3: "jasonborne"
     }
   });
+  const [userState, setUserState] = useState({
+      _id: "",
+      update: (_id) => {
+        console.log(_id)
+        setUserState({...userState, _id})
+      }
+  })
 
 
   return (
     <Router>
       <div>
         <Wrapper>
-          {/* <DeveloperContext.Provider value={developerState}> */}
+          <UserContext.Provider value={userState}>
           <Route exact path="/" component={HomePageRender} />
           <Route exact path="/create" component={CharacterCreateAccMenu} />
           <Route exact path="/roster" component={Roster} />
@@ -34,7 +41,7 @@ function App() {
           <Route exact path="/api/test" component={TestAPI} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
-          {/* </DeveloperContext.Provider> */}
+          </UserContext.Provider>
         </Wrapper>
       </div>
     </Router>
