f1e8f0115399358cb9fb12a3943bddb12b8043af
Ezekiel <zekejenyart@gmail.com>
2020-08-22 11:05:21 -0500

    Added client/build parameter

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('client/build'));
+}
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
